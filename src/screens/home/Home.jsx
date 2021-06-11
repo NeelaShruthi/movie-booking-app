57d7dda0210a28b6f71af0f9ea9398c1de9027e9
NeelaShruthi <shruthineela205@gmail.com>
2021-06-11 23:20:40 +0530

    checkpoint 4

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -3,6 +3,8 @@ import "./Home.css";
 import Header from "../../common/header/Header";
 import GridListBanner from "../../common/GridListBanner";
 import Movielistdisplay from "../../common/Movielistdisplay";
+import MovieFilter from "../../common/MovieFilter";
+
 
 class Home extends React.Component {
     render() {
@@ -18,7 +20,7 @@ class Home extends React.Component {
                         <Movielistdisplay />
                     </div>
                     <div className="right">
-
+                         <MovieFilter />
                     </div>
                 </div>
             </div>
